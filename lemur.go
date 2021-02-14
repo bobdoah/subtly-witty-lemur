8b7975777988eaccad4193cd41f6917302beeb5e
Robert Williams <robertwilliams1985@gmail.com>
2021-02-14 18:16:05 +0000

    Decode postcodes to latitude, longitude

--- lemur.go
+++ lemur.go
@@ -6,9 +6,20 @@ import (
 	"os"
 	"time"
 
+	"github.com/go-resty/resty/v2"
 	"github.com/philhofer/tcx"
 	"github.com/urfave/cli/v2"
 )
+
+type latlon struct {
+	Longitude float64 `json:"longitude"`
+	Latitude  float64 `json:"latitude"`
+}
+
+type result struct {
+	Status int    `json:"status"`
+	Latlon latlon `json:"result"`
+}
 
 func readTcx(filepath string) error {
 	db, err := tcx.ReadFile(filepath)
@@ -24,10 +35,46 @@ func readTcx(filepath string) error {
 	return nil
 }
 
+func getLatLongFromPostcode(postcode string, coords *latlon) error {
+	client := resty.New()
+	resp, err := client.R().
+		SetHeader("Accept", "application/json").
+		SetResult(&result{}).
+		Get("https://api.postcodes.io/postcodes/" + postcode)
+	if err != nil {
+		return err
+	}
+	response := resp.Result().(*result)
+	*coords = response.Latlon
+	return nil
+}
+
+func printLatLons(postcodes []string) error {
+	for _, postcode := range postcodes {
+		coords := latlon{}
+		err := getLatLongFromPostcode(postcode, &coords)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Postcode: %s is at lat: %f, lon: %f\n", postcode, coords.Latitude, coords.Longitude)
+	}
+	return nil
+}
+
 func main() {
 	app := &cli.App{
-		Name:  "greet",
-		Usage: "fight the loneliness!",
+		Name:  "upload-tcx",
+		Usage: "upload a tcx file to somewhere",
+		Flags: []cli.Flag{
+			&cli.StringSliceFlag{
+				Name:  "home",
+				Usage: "Postcodes of home starting points",
+			},
+			&cli.StringSliceFlag{
+				Name:  "work",
+				Usage: "Postcodes of work starting points",
+			},
+		},
 		Action: func(c *cli.Context) error {
 			if c.NArg() > 0 {
 				var i int
@@ -35,7 +82,7 @@ func main() {
 					readTcx(c.Args().Get(i))
 				}
 			}
-			return nil
+			return printLatLons(c.StringSlice("home"))
 		},
 	}
 
