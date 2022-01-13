6d332b1d5a5e3ba32528bc63e0707dde6da5bbff
Robert Williams <robertwilliams1985@gmail.com>
2022-01-13 21:22:50 +0000

    Add a parser for the endomondo JSON workouts
    Need to figure out what a workout type was. It's in the JSON files
    supplied with Endomondo, but not supported by the TCX arg.

---
+++ jsonfile/jsonfile.go
@@ -0,0 +1,55 @@
+package jsonfile
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"time"
+)
+
+// Workout represents a workout in the JSON file
+type Workout struct {
+	Name        string    `json:"name"`
+	Sport       string    `json:"sport"`
+	Source      string    `json:"source"`
+	CreatedDate time.Time `json:"created_date"`
+	StartTime   time.Time `json:"start_time"`
+	EndTime     time.Time `json:"end_time"`
+	Duration    float64   `json:"duaration_s"`
+	Distance    float64   `json:"distance_km"`
+	Calories    float64   `json:"calories_kcal"`
+	AltitudeMin float64   `json:"altitude_min_m"`
+	AltitudeMax float64   `json:"altitude_max_m"`
+	Speed       float64   `json:"speed_avg_kmh"`
+	Ascend      float64   `json:"ascend_m"`
+	Descend     float64   `json:"descend_m"`
+	Points      [][]Point `json:"points"`
+}
+
+// Point represents a geographic point recorded during the workout
+type Point struct {
+	Location  [][]Location `json:"location,omitempty"`
+	Altitude  float64      `json:"altitude,omitempty"`
+	Timestamp string       `json:"timestamp,omitempty"`
+}
+
+// Location represents a geographic location in latitude and longitude
+type Location struct {
+	Latitude  float64 `json:"latitude,omitempty"`
+	Longitude float64 `json:"longitude,omitempty"`
+}
+
+func readJsonfile(filename *string) (*[]Workout, error) {
+	jsonfile, err := os.Open(*filename)
+	defer jsonfile.Close()
+	if err != nil {
+		return nil, err
+	}
+	bytes, err := ioutil.ReadAll(jsonfile)
+	if err != nil {
+		return nil, err
+	}
+	var workouts []Workout
+	json.Unmarshal(bytes, &workouts)
+	return &workouts, nil
+}
